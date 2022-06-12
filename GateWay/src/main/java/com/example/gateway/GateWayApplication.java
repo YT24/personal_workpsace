5cbf325415e6eee292dd4af78618ee890f5e0d1c
yangte <yangte@entropyreduce.com>
2022-06-12 15:19:17 +0800

    commit-001 + 002

--- GateWay/src/main/java/com/example/gateway/GateWayApplication.java
+++ GateWay/src/main/java/com/example/gateway/GateWayApplication.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class GateWayApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GateWayApplication.class, args);
+        System.out.println(1);
     }
 
 
